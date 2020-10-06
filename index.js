b443b416872dfc216ffe6232d2ca9c238154b2bd
Max <maxi.guenther98@web.de>
2020-10-06 22:16:38 +0200

    add hidden class to button

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var el = document.getElementById("tollesBild");
+
+var button= document.getElementsByClassName("redButton")[0]
+
+console.log(button)
+
 function showImage() {
     el.removeAttribute("class")
+    button.classList.add("hidden")
 }
